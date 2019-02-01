6d9c41c881ab070cd2c7b844e0ca7fa684107122
JohnPolich <stjnp20@moravian.edu>
2019-02-01 13:12:56 -0500

    Implement tests with floating point values

--- test_grades.py
+++ test_grades.py
@@ -13,5 +13,5 @@ def test_single_grade():
 
 
 def test_two_grades():
-    grades = [45,55]
-    assert compute_hw_average(grades) == 50
+    grades = [50,51]
+    assert compute_hw_average(grades) == 50.5
